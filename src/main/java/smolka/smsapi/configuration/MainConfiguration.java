5872301d043243d0a31d04a25b1848c62454cfe9
ihsmokesen <itsmokesmm@gmail.com>
2020-12-04 21:24:24 +0300

    init spring-boot and logger

---
+++ src/main/java/smolka/smsapi/configuration/MainConfiguration.java
@@ -0,0 +1,11 @@
+package smolka.smsapi.configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableWebMvc
+public class MainConfiguration {
+}
