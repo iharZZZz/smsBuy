f425b67dabbb586630d22000087ffdd20e5a94d0
ihsmokesen <itsmokesmm@gmail.com>
2021-02-28 14:07:54 +0300

    добавил валидацию, поменял аннотации для валидации на нормальные

--- src/main/java/smolka/smsapi/dto/input/OrderRequest.java
+++ src/main/java/smolka/smsapi/dto/input/OrderRequest.java
@@ -1,11 +1,11 @@
 package smolka.smsapi.dto.input;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
